f06ce1497cc15d71951fd0826c1dff79b2ad165f
Javier Romera Claros <Javi@MacBook-Air-de-Montserrat.local>
2025-01-17 15:44:32 +0100

    setup difficulty selection #1

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from 'react'
+
+import Board from './components/Board'
+
 function App() {
-  return <h1></h1>
+  const [diff, setDiff] = useState(null)
+
+  const handleDiffSelected = (event) => {
+    const { id } = event.target
+
+    setDiff(id)
+  }
+
+  return (
+    <main className="w-full h-full flex justify-center items-center">
+      {!diff ?
+        <div className="flex gap-[40px] font-bold flex-col items-center justify-center">
+          <p className="text-[#F8F8FF] ">Choose difficulty</p>
+
+          <div className="flex gap-[10px]">
+            <button id="easy" onClick={handleDiffSelected} className="w-[110px] border-[#D3D3D3] bg-[#F8F8FF] border-2 rounded-[2px] hover:bg-[#D3D3D3]">Easy</button>
+            <button id="medium" onClick={handleDiffSelected} className="w-[110px] border-[#D3D3D3] bg-[#F8F8FF] border-2 rounded-[2px] hover:bg-[#D3D3D3]">Medium</button>
+            <button id="hard" onClick={handleDiffSelected} className="w-[110px] border-[#D3D3D3] bg-[#F8F8FF] border-2 rounded-[2px] hover:bg-[#D3D3D3]">Hard</button>
+          </div>
+        </div>
+        : <div>
+          <Board diff={diff}>
+
+          </Board>
+        </div>
+      }
+    </main>
+  )
 }
 
 export default App
---
+++ app/src/components/Board.jsx
@@ -0,0 +1,7 @@
+function Board(props) {
+    const { diff } = props
+
+    return <h1>{diff}</h1>
+}
+
+export default Board
